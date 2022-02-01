a0433252ccfc862e7cc6738f5458bea3033f3ae2
thiagofcm <thiagof1503@gmail.com>
2022-02-01 19:50:07 -0300

    commit 1

---
+++ caAlunoMateria/Aluno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace caAlunoMateria
+{
+    class Aluno
+    {
+        // atributos
+        private String nome;
+        private String numero_matricula;
+        private int periodo;
+        int qtd_materias;
+        Materia[] lista_materias = new Materia[8];
+
+
+
+        // metodos
+
+        public void adicionarMateria(Materia m) {
+            lista_materias[qtd_materias] = new Materia(m.getNome(), m.getCodigo());
+            qtd_materias++;
+        }
+        public Aluno() { // construtor default
+            nome = " ";
+            numero_matricula = " ";
+            periodo = 0;
+            qtd_materias = 0;
+        }
+
+        public Aluno(String _nome, String _numero_matricula, int _periodo) {
+            nome = _nome;
+            numero_matricula = _numero_matricula;
+            periodo = _periodo;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+        public void setNome(String _nome) {
+            nome = _nome;
+        }
+         
+    } // fim da classe aluno
+}
---
+++ caAlunoMateria/Materia.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace caAlunoMateria
+{
+    class Materia
+    {
+        // atributos
+        private String nome;
+        private String codigo;
+
+
+        // metodos
+        public Materia() { // construtor default
+            nome = " ";
+            codigo = " ";
+        }
+        public Materia(String _nome, String _codigo) { // construtor
+            nome = _nome;
+            codigo = _codigo;
+        } 
+        public String getNome() {
+            return nome;
+        }
+        public void setNome( String _nome) {
+            nome = _nome;
+        }
+        public String getCodigo() {
+            return codigo;
+        }
+        public void setCodigo(String _codigo) {
+            codigo = _codigo;
+        }
+    } // fim da classe materia
+}
